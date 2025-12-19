22f7ec8943094ecd925956e97b77beb378503960
phgodoy <phgodoy12@gmail.com>
2025-12-18 21:35:39 -0300

    Update .gitgnore

---
+++ BookStoreFlyweight/Program.cs
@@ -0,0 +1,29 @@
+﻿using Flyweight.Copy;
+using Flyweight.Factory;
+
+class Program
+{
+    static void Main()
+    {
+        var factory = new BookFactory();
+
+        var book1 = factory.GetBook(
+            "Clean Architecture",
+            "Robert C. Martin",
+            "Pearson");
+
+        var book2 = factory.GetBook(
+            "Clean Architecture",
+            "Robert C. Martin",
+            "Pearson");
+
+        var copy1 = new BookCopy(1, book1);
+        var copy2 = new BookCopy(2, book2);
+
+        copy1.Display();
+        copy2.Display();
+
+        Console.WriteLine(
+            $"Mesma instância? {ReferenceEquals(book1, book2)}");
+    }
+}
